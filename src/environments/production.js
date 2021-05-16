a639fda5fc84ed52c728648ed4785186241b58ea
Matheus Batista <matheus_roberto_batista@hotmail.com>
2021-05-16 18:10:00 -0300

    remove netlify.toml

--- src/environments/production.js
+++ src/environments/production.js
@@ -2,6 +2,8 @@ import environment from './base'
 import externalConfig from './external-config'
 import merge from 'lodash/merge'
 
+console.log(process.env, externalConfig)
+
 const env = environment()
 const productionEnv = merge(env, externalConfig)
 export default productionEnv
